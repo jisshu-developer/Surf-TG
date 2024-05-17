057ed377728955d86d95471a16c3f618af9bf60d
Jisshu developer <143610535+jisshu-developer@users.noreply.github.com>
2024-05-17 16:55:32 +0530

    Update jisshu.py

--- bot/jisshu.py
+++ bot/jisshu.py
@@ -1,6 +1,6 @@
 from os import getenv
 
 class Telegramm:
-   AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL").split(",")]
-   ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "surfTG")
+   AUTH_CHANNEL = [channel.strip() for channel in getenv("AUTH_CHANNEL").split("-1002107792448, -1001942980809")]
+   ADMIN_PASSWORD = getenv("ADMIN_PASSWORD", "zkhan111@")
    THEME = getenv("THEME", "quartz").lower()
